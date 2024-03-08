df0d6ce6734bd0b4f516b7b8cbaafd9079ceae6b
Mehdi Cherifi <73710391+cherifimehdi@users.noreply.github.com>
2024-03-08 17:31:28 +0100

    Add files via upload
    Script that allow you to retrieve OSPF neighbors state for IPv4 (Default) and/or IPv6

---
+++ Images/Hybrid_OSPF_Neighbors.py
@@ -0,0 +1,23 @@
+def hybrid_neighbors(device, ip='ipv4'):
+    # Connect to R1 via SSH
+    net_connect = ConnectHandler(**device)
+    # Retrieve the parsed result of "show ip ospf neighbor" command
+    output = net_connect.send_command(f"show {'ipv6' if ip!='ipv4' else 'ip'} ospf neighbor")
+    # Display the result
+    print(output)
+    # End the SSH session for R1
+    net_connect.disconnect()
+    return print(f"This is {ip} data")
+
+if __name__ == "__main__":
+    # Import the ConnectHandler Function to create an Object for R1
+    from netmiko import ConnectHandler
+    # Parameters of R1
+    password = "cisco"
+    R1 = {
+    "device_type": "cisco_ios",
+    "host": "192.168.1.251",
+    "username": "Admin",
+    "password": password,
+    }
+    hybrid_neighbors(R1, ip='ipv6')
